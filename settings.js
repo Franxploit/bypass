a78ab6da3dd11a4baaab74a24f5816bc80eff6ad
Franxploit <akunbusines69@gmail.com>
2025-11-07 05:24:37 +0800

    Upload settings.js

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  ownerName: 'ストレッサーDDOS', // Ganti dengan nama owner lu
+  botToken: '7488413334:AAHVeUWH3y_WBqIwLmi1iJ3VBhbxsBuBUcE', // Ganti dengan token bot Telegram lu
+  owner: '5393907859', //OWNER user id
+};
+
+module.exports = settings;
